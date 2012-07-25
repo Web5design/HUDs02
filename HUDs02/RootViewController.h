d77afab2ec9c7ea5c140cde65a156a4cb8e8473f
Conrado Andreu Capdevila <conrado.andreu@gmail.com>
2012-07-25 16:50:57 +0200

    Two views in the XIB of main controller

---
+++ HUDs02/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  HUDs02
+//
+//  Created by Conrado Andreu Capdevila on 7/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UIView *hudActivationViewRef;
+@property (nonatomic, weak) IBOutlet UIView *childContainerViewRef;
+
+@end
